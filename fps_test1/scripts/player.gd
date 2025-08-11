ae835de1878aa0d7eed5fd528f9acb483df22491
Daniel G. <nuclearrocketcar@gmail.com>
2025-08-11 17:20:09 -0400

    Add fist viewmodel texture and update main scene
    Added a new fist viewmodel texture and its import file. Updated main.tscn to use the new texture in a CanvasLayer under the player's camera, and adjusted positions of several entities and objects. Also fixed input mapping in project.godot by renaming 'crouch' to 'jump'.

--- fps_test1/scripts/player.gd
+++ fps_test1/scripts/player.gd
@@ -11,23 +11,17 @@ extends CharacterBody3D
 @onready var camera_3d: Camera3D = $neck/head/eyes/Camera3D
 @onready var animation_player: AnimationPlayer = $neck/head/eyes/AnimationPlayer
 @onready var interactcast: RayCast3D = $neck/head/eyes/interactcast
-@onready var texture_rect: TextureRect = $neck/head/eyes/CanvasLayer/Control/TextureRect
-
+@onready var texture_rect: TextureRect = $neck/head/eyes/Camera3D/CanvasLayer2/Control/TextureRect
 
 
 # Debug States
-
 var debug_enabled = false
 
 # in order: crouch/slide, jump, wallrun, wallkick
-
-
 var crouch_enabled = true
 var jump_enabled = true
 var wallrun_enabled = true
 var wallkick_enabled = true
-
-
 
 # States
 var walking := false
@@ -36,6 +30,9 @@ var crouched := false
 var freelook := false
 var sliding := false
 var was_in_air = false
+
+# NEW: Centralized crouch state management
+var want_to_stand := false  # Player wants to stand but ceiling prevents it
 
 # Slide vars
 var slide_timer = 0.0
@@ -64,8 +61,8 @@ var max_wallrun_time = 3.0
 var wallrun_velocity_set = false
 
 # Wallkick vars
-@export var wall_kick_strength_horiz = 4  # How far away?
-@export var wall_kick_strength_vert = 5 # How high?
+@export var wall_kick_strength_horiz = 4
+@export var wall_kick_strength_vert = 5
 
 # Headbob vars
 const headbob_sprint_speed = 22
@@ -93,8 +90,8 @@ var air_lerp = 3
 var freelook_angle = 8
 
 # Air movement vars
-@export var air_control_force = 10.0  # adjust to how responsive air movement is
-@export var max_air_speed = 7.5  # Maximum horizontal speed in the air
+@export var air_control_force = 10.0
+@export var max_air_speed = 7.5
 
 # Input vars
 var direction = Vector3.ZERO
@@ -105,30 +102,11 @@ var mouseinput := true
 var lastInteraction
 var is_holding = false
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
-	
 	# Feature enablers
-	
 	if event.is_action_pressed("debug_abilities"):
 		debug_enabled = !debug_enabled
 		
@@ -145,7 +123,6 @@ func _input(event):
 			wallkick_enabled = true
 			print("Bye bye debug!")
 	
-	
 	if event.is_action_pressed("num1") and debug_enabled:
 		crouch_enabled = true
 		print("crouch enabled!")
@@ -161,8 +138,6 @@ func _input(event):
 	if event.is_action_pressed("num4") and debug_enabled:
 		wallkick_enabled = true
 		print("wallkick enabled!")
-	
-	
 	
 	# Mouse move logic
 	if event.is_action_pressed("esc"):
@@ -185,10 +160,7 @@ func _input(event):
 			head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 			head.rotation.x = clamp(head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
-func _physics_process(delta: float) -> void:	
-	# Handle wall system timers
-	#handle_wall_timers(delta)
-	
+func _physics_process(delta: float) -> void:
 	# Getting movement input
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	horizontal_velocity = Vector2(velocity.x, velocity.z)
@@ -200,6 +172,8 @@ func _physics_process(delta: float) -> void:
 		print("sprinting:", sprinting)
 		print("velocity.y:", velocity.y)
 		print("horiz velocity", horizontal_velocity.length())
+		print("want_to_stand:", want_to_stand)
+		print("crouched:", crouched)
 	
 	if Input.is_action_just_pressed("reset"):
 		global_position = Vector3.ZERO
@@ -208,14 +182,14 @@ func _physics_process(delta: float) -> void:
 		head.rotation = Vector3.ZERO
 		reset_wall_state()
 	
-	# Handle movement states (crouching, sprinting, etc.)
-	handle_movement_states(delta, input_dir)
+	# Handle movement states - NEW STREAMLINED VERSION
+	handle_movement_states_new(delta, input_dir)
 	
 	# Handle freelook
 	handle_freelook(delta)
 	
 	# Handle sliding
-	handle_sliding(delta)
+	handle_sliding_new(delta)
 	
 	# Handle headbob
 	handle_headbob(delta, input_dir)
@@ -233,6 +207,7 @@ func _physics_process(delta: float) -> void:
 	
 	last_velocity = velocity
 	move_and_slide()
+	
 	# Check for air-to-ground transition slide
 	handle_air_to_slide_transition(was_airborne)
 	
@@ -243,12 +218,134 @@ func _physics_process(delta: float) -> void:
 	
 	# Handle viewmodel stuff
 	handle_viewmodel(lastInteraction)
+
+# NEW: Streamlined movement state handling
+func handle_movement_states_new(delta: float, input_dir: Vector2):
+	"""Unified movement state handling with proper ceiling checks"""
 	
+	# Check if player wants to crouch or stand
+	var jump_input_pressed = Input.is_action_pressed("jump")
+	var ceiling_blocked = ray_cast_3d.is_colliding()
 	
+	# Check for slide initiation BEFORE changing states
+	# This preserves the sprinting state check
+	if (is_on_floor() and crouch_enabled and jump_input_pressed and not sliding and 
+		horizontal_velocity.length() > 7 and sprinting and input_dir != Vector2.ZERO):
+		start_slide(input_dir)
+	
+	# Determine desired crouch state
+	var should_be_crouched = false
+	
+	if is_on_floor() and crouch_enabled and (jump_input_pressed or sliding):
+		should_be_crouched = true
+		want_to_stand = false
+	else:
+		# Player wants to stand
+		want_to_stand = true
+		if ceiling_blocked:
+			# Can't stand due to ceiling, stay crouched
+			should_be_crouched = true
+		else:
+			# Can stand freely
+			should_be_crouched = false
+	
+	# Apply crouch state
+	if should_be_crouched:
+		apply_crouch_state(delta)
+		
+		# Handle jump charging when crouched and on ground
+		if is_on_floor() and jump_input_pressed:
+			is_charging = true
+			crouch_counter += delta * 10.0
+			crouch_counter = clamp(crouch_counter, min_crouch_counter, max_crouch_counter)
+			
+	else:
+		apply_stand_state(delta)
+		
+		# Handle jump release when standing up
+		if Input.is_action_just_released("jump") and jump_enabled and is_on_floor() and is_charging:
+			do_jump(crouch_counter)
+		elif Input.is_action_pressed("sprint") and is_on_floor():
+			# Sprinting
+			current_speed = lerp(current_speed, sprint_speed, delta * lerp_speed/4)
+			if horizontal_velocity.length() > 7:
+				walking = false
+				sprinting = true
+				crouched = false
+		else:
+			# Walking
+			current_speed = lerp(current_speed, walking_speed, delta * lerp_speed)
+			walking = true
+			sprinting = false
+			crouched = false
 
-#func handle_wall_timers(delta: float):
+func apply_crouch_state(delta: float):
+	"""Apply crouched state consistently"""
+	current_speed = lerp(current_speed, crouch_speed, delta * lerp_speed)
+	head.position.y = lerp(head.position.y, crouch_depth, delta * lerp_speed)
+	standing_collision.disabled = true
+	crouched_collision.disabled = false
+	
+	walking = false
+	sprinting = false
+	crouched = true
 
+func apply_stand_state(delta: float):
+	"""Apply standing state consistently"""
+	head.position.y = lerp(head.position.y, 0.0, delta * lerp_speed)
+	standing_collision.disabled = false
+	crouched_collision.disabled = true
+	
+	# Reset crouch-related vars when successfully standing
+	if not crouched:
+		crouch_counter = 0.0
+		is_charging = false
 
+func start_slide(input_dir: Vector2):
+	"""Initiate sliding"""
+	if not sliding:
+		sliding = true
+		slide_timer = slide_timer_max
+		slide_vector = input_dir
+		freelook = true
+		print("slide begin")
+
+# NEW: Streamlined sliding handler
+func handle_sliding_new(delta: float):
+	"""Handle sliding with proper ceiling checks"""
+	if not sliding:
+		return
+		
+	slide_timer -= delta
+	
+	# Check if slide should end
+	var should_end_slide = false
+	var should_jump = false
+	
+	if slide_timer <= 0:
+		print("slide end via timer")
+		should_end_slide = true
+	elif Input.is_action_just_released("jump"):
+		print("slide end via jump release")
+		should_end_slide = true
+		if jump_enabled and is_charging:
+			should_jump = true
+	
+	if should_end_slide:
+		sliding = false
+		freelook = false
+		
+		if should_jump:
+			# Only jump if we can stand (no ceiling)
+			if not ray_cast_3d.is_colliding():
+				do_jump(crouch_counter)
+			else:
+				print("Can't jump due to ceiling, staying crouched")
+		
+		# Note: Crouch state will be handled by handle_movement_states_new()
+		# which will respect ceiling checks automatically
+
+# Keep all the other functions the same...
 func handle_wall_system(delta: float, _input_dir: Vector2):
 	"""Centralized wall system handling"""
 	var touching_wall = is_on_wall_only()
@@ -286,7 +383,6 @@ func perform_wallkick():
 		# Apply strong force away from wall
 		velocity += wall_normal * wall_kick_strength_horiz
 		velocity.y = wall_kick_strength_vert
-		#direction = Vector3.ZERO #this just halts
 		
 func reset_wall_state():
 	"""Reset all wall-related state"""
@@ -296,60 +392,6 @@ func reset_wall_state():
 
 func is_wallrunning() -> bool:
 	return wall_state == WallState.WALLRUNNING
-
-func handle_movement_states(delta: float, input_dir: Vector2):
-	"""Handle crouching, sprinting, walking states"""
-	# Crouching
-	if is_on_floor() and crouch_enabled and Input.is_action_pressed("crouch") or sliding:
-		
-		# Do crouch stuff
-		
-		current_speed = lerp(current_speed, crouch_speed, delta * lerp_speed)
-		head.position.y = lerp(head.position.y, crouch_depth, delta * lerp_speed)
-		standing_collision.disabled = true
-		crouched_collision.disabled = false
-		
-		# If touching floor, charge jump to  be used later
-		
-		if is_on_floor():
-			is_charging = true
-			crouch_counter += delta * 10.0
-			crouch_counter = clamp(crouch_counter, min_crouch_counter, max_crouch_counter)
-		
-		# Slide begin logic
-		if (horizontal_velocity.length() > 7 and sprinting and input_dir != Vector2.ZERO and is_on_floor()):
-			sliding = true
-			slide_timer = slide_timer_max
-			slide_vector = input_dir
-			freelook = true
-			print("slide begin")
-			
-		walking = false
-		sprinting = false
-		crouched = true
-		
-	elif ray_cast_3d.is_colliding():	# If im under something!
-		crouch_counter = 0.0
-	elif !ray_cast_3d.is_colliding():	# If nothing above me
-		# Uncrouching / Standing
-		crouch(1, delta)
-		
-		if Input.is_action_just_released("crouch") and jump_enabled and is_on_floor():
-			do_jump(crouch_counter)
-		elif Input.is_action_pressed("sprint"):
-			# Sprinting
-			if is_on_floor():
-				current_speed = lerp(current_speed, sprint_speed, delta * lerp_speed/4)
-				if horizontal_velocity.length() > 7:
-					walking = false
-					sprinting = true
-					crouched = false
-		else:
-			# Walking
-			current_speed = lerp(current_speed, walking_speed, delta * lerp_speed)
-			walking = true
-			sprinting = false
-			crouched = true
 
 func handle_freelook(delta: float):
 	"""Handle freelook camera behavior"""
@@ -363,23 +405,6 @@ func handle_freelook(delta: float):
 		freelook = false
 		neck.rotation.y = lerp(neck.rotation.y, 0.0, delta * lerp_speed * 2.6)
 		eyes.rotation.z = lerp(eyes.rotation.z, 0.0, delta * lerp_speed * 2.6)
-
-func handle_sliding(delta: float):
-	"""Handle sliding mechanics"""
-	if sliding:		
-		slide_timer -= delta
-		if slide_timer <= 0:
-			print("slide end via timer")
-			sliding = false
-			freelook = false
-		elif Input.is_action_just_released("crouch"):
-			if jump_enabled:
-				print("slide end via jump release")
-				freelook = false
-				do_jump(crouch_counter)
-			else:
-				print("uncrouch!")
-				crouch(0, delta)
 
 func handle_headbob(delta: float, input_dir: Vector2):
 	"""Handle camera headbob"""
@@ -427,7 +452,6 @@ func handle_player_movement(delta: float, input_dir: Vector2):
 		if input_dir != Vector2.ZERO:
 			var input_vector = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 			
-			
 			# Add steering force to existing velocity instead of replacing it
 			velocity.x += input_vector.x * air_control_force * delta
 			velocity.z += input_vector.z * air_control_force * delta
@@ -471,41 +495,24 @@ func handle_air_to_slide_transition(was_airborne: bool):
 		var horizontal_speed = Vector2(velocity.x, velocity.z).length()
 		
 		# If moving fast enough, holding crouch, and have horizontal momentum
-		if horizontal_speed >= sprint_speed/2 and Input.is_action_pressed("crouch"):
+		if horizontal_speed >= sprint_speed/2 and Input.is_action_pressed("jump"):
 			print("=== AIR-TO-SLIDE TRANSITION ===")
 			print("Landing speed:", horizontal_speed)
-
 			
 			# Convert velocity to local space slide vector (matching coordinate system)
 			var world_velocity_normalized = Vector3(velocity.x, 0, velocity.z).normalized()
 			var local_velocity = transform.basis.inverse() * world_velocity_normalized
 			slide_vector = Vector2(local_velocity.x, local_velocity.z)
 			
-			# Start sliding
-			sliding = true
-			slide_timer = slide_timer_max
+			# Start sliding using the new unified function
+			start_slide(slide_vector)
 			
-			# Enable freelook for slide
-			freelook = true
-			
-			# Set appropriate collision and states
-			standing_collision.disabled = true
-			
-			
+			# Set appropriate states
 			crouched = true
 			walking = false
 			sprinting = false
-			
-			
-			
 
 func handle_interactions_raycast() -> Object:
-	#if interactcast.is_colliding():
-		#var collider = interactcast.get_collider()
-		#print(collider.name)
-		#if collider and collider.name == "InteractBox" and Input.is_action_just_pressed("use"):
-			#toggle_interactbox(collider)
-			
 	if interactcast.is_colliding():
 		
 		var hit = interactcast.get_collider()
@@ -513,36 +520,34 @@ func handle_interactions_raycast() -> Object:
 		
 		if lastInteraction:
 			print("last interactable object:", lastInteraction.name)
-		if !is_holding and hit and hit.name.begins_with("obj") and Input.is_action_just_pressed("use"):	#checks if i interacted with interactBox, to be changed
-			lastInteraction = hit	#this is to differenciate what im looking at with what i just interacted with. this is to then be able to use said object onto something else.
+		if !is_holding and hit and hit.name.begins_with("obj") and Input.is_action_just_pressed("use"):
+			lastInteraction = hit
 			toggle_interactbox(lastInteraction, 0)
 			is_holding = true
 			
 		elif is_floor(normal) and is_holding and lastInteraction and Input.is_action_just_pressed("use") and !lastInteraction.visible:
 			print(lastInteraction.name, " should move here!")
-			print("looking at ", hit.name)		#print the name of the thing colliding
-			if lastInteraction.name.begins_with("obj_") and hit.name.begins_with("ent_"):	#giving animal fruit
-				print(lastInteraction.name, " given to ", hit.name)	#this is dynamic for the other animals to useas well
+			print("looking at ", hit.name)
+			if lastInteraction.name.begins_with("obj_") and hit.name.begins_with("ent_"):
+				print(lastInteraction.name, " given to ", hit.name)
 				if lastInteraction.name.ends_with("apple") and hit.name.ends_with("cow"):
-					#this is when you feed the cow the apple. you gain the ability to jump.
 					crouch_enabled = true
-					is_holding = false	#the animal ate it!
+					is_holding = false
 					hit.play_sound()
 				elif lastInteraction.name.ends_with("banana") and hit.name.ends_with("fox"):
 					jump_enabled = true
 					hit.play_sound()
-					is_holding = false	#the animal ate it!
+					is_holding = false
 				elif lastInteraction.name.ends_with("grape") and hit.name.ends_with("chimp"):
 					wallrun_enabled = true
 					hit.play_sound()
-					is_holding = false	#the animal ate it!
+					is_holding = false
 				
 			else:
 				move_interactbox_to_floor(interactcast.get_collision_point(), lastInteraction)
-			lastInteraction = null	#this is outside the if else so any sort of interaction at all will just remove the object.
+			lastInteraction = null
 	
-	return lastInteraction	#returns last object interacted with
-	
+	return lastInteraction
 
 func toggle_interactbox(box: Node, toggle: bool):
 	var shape = box.get_node_or_null("CollisionShape3D")
@@ -554,44 +559,25 @@ func toggle_interactbox(box: Node, toggle: bool):
 		shape.disabled = true
 
 func is_floor(collider: Vector3) -> bool:
-	# Option 1: use node name
-	if collider.y == 1:	#if the normal points up!
+	if collider.y == 1:
 		return true
 	else: return false
 
 func move_interactbox_to_floor(point: Vector3, box: Node):	
 	var new_position = box.global_position
 	new_position.x = point.x
-	new_position.y = point.y	#this is a terrible way to solve this issue, only works for the box for now, should probably fix this later.
+	new_position.y = point.y
 	new_position.z = point.z
-	# Optionally snap Y to ground height
 	box.global_position = new_position
 	
 	toggle_interactbox(box, 1)  # Make it reappear
 	is_holding = false
 
 func handle_viewmodel(object: Object):
-	#idea here is to pass thru what object is being held, then change the viewmodel depending on that
-	#FOR LATER: show the icon of the animal that fruit should be given to
 	if lastInteraction:
 		print("holding ", lastInteraction.name)
-		texture_rect.visible = true	#supposed to be 2d viewmodel
+		texture_rect.visible = true
 	elif !lastInteraction:
 		print("holding nothing")
 		texture_rect.visible = false
 	return
-	
-func crouch(crouch: bool, delta):
-	if crouch:
-		crouched = true
-		head.position.y = lerp(head.position.y, 0.0, delta * lerp_speed)
-		standing_collision.disabled = false
-		crouched_collision.disabled = true
-		
-	else:
-		crouched = false
-		current_speed = lerp(current_speed, crouch_speed, delta * lerp_speed)
-		head.position.y = lerp(head.position.y, crouch_depth, delta * lerp_speed)
-		standing_collision.disabled = true
-		crouched_collision.disabled = false
-		print("UNcrouch via function")
