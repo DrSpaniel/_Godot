fc6f6574388b5ae00a7774a7ab237353a8119bd6
Daniel G. <nuclearrocketcar@gmail.com>
2025-08-20 15:29:34 -0400

    did some cool stuff
    made a weird cool shader! also just realized that anything can be a scene. epic

--- shaderstuff/guy_4.gd
+++ shaderstuff/guy_4.gd
@@ -1,8 +1,5 @@
 extends Sprite2D
 
-
-
-
 func _on_h_slider_value_changed(value: float) -> void:
 	material.set_shader_parameter("amount", int(value))
-	pass # Replace with function body.
+	print(value)
